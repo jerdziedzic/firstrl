5a582b2660723686b2e5e1db453f02162d6b78a9
jerdziedzic <jdziedzic@gmail.com>
2025-12-23 12:30:25 -0600

    Chapter 2, just implemented engine.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 import tcod
 
-# Import functions from actions.py, entity,py, and input_handlers.py
-from actions import EscapeAction, MovementAction
+# Import functions from engine.py, entity,py, and input_handlers.py
+from engine import Engine
 from entity import Entity
 from input_handlers import EventHandler
 
@@ -24,6 +24,8 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 0))
     entities = {npc, player} # Set that will eventually hold all entities on the map
 
+    engine = Engine(entities=entities, event_handler=event_handler, player=player)
+
     # Create the screen and pass size parameters, tileset, title, and vsync boolean
     with tcod.context.new_terminal(
         screen_width,
@@ -35,30 +37,13 @@ def main() -> None:
         root_console = tcod.Console(screen_width, screen_height, order="F") # Creates console to draw to; F flips y,x to x,y
         # Start of main game loop
         while True:
-            root_console.print(x=player.x, y=player.y, string=player.char, fg=player.color) # Pass player starting coordinates, character, and color
+            engine.render(console=root_console, context=context)
+ 
+            events = tcod.event.wait()
 
-            context.present(root_console)
+            engine.handle_events(events)
 
-            root_console.clear() # Clear the console after drawing to it (to eliminate leftover drawn characters from movement)
-
-            # Allows us to quit by clicking upper-right X
-            for event in tcod.event.wait():
-
-                # Send an event to the event handler's dispatch method (which sends an event to its proper place)
-                action = event_handler.dispatch(event)
-
-                if action is None: # If no key is pressed or the pressed key is unrecognized, skip over the rest of the loop
-                    continue
-
-                # Add -1, 0, or +1 to player_x and/or player_y to move the character around
-                if isinstance(action, MovementAction):
-                    player.move(dx=action.dx, dy=action.dy) # Entity class handles movement
-
-                # If Esc is pressed, exit the program
-                elif isinstance(action, EscapeAction):
-                    raise SystemExit()
-
-
+            
 # Only run the main function when we explicitly run the script
 if __name__ == "__main__":
     main()
