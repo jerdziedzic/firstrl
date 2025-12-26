92ca5dd9c2f78f7b1019655889bb6645f968eba5
jerdziedzic <jdziedzic@gmail.com>
2025-12-26 11:55:54 -0600

    Finished Chapter 4

--- engine.py
+++ engine.py
@@ -2,6 +2,7 @@ from typing import Set, Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
+from tcod.map import compute_fov
 
 from entity import Entity
 from game_map import GameMap
@@ -13,6 +14,7 @@ class Engine:
         self.event_handler = event_handler # Same event handler from main.py; handles events
         self.game_map = game_map
         self.player = player # Player entity; we have a separate reference outside of entities for ease of access (since we'll work with it a lot)
+        self.update_fov()
 
     # Basically same event handler we originally added to main.py
     def handle_events(self, events: Iterable[Any]) -> None:
@@ -24,6 +26,18 @@ class Engine:
 
             action.perform(self, self.player)
 
+            self.update_fov() # Update the FOV before the player's next action
+
+    def update_fov(self) -> None:
+        """Recompute the visible area based on the player's point of view"""
+        self.game_map.visible[:] = compute_fov( # Set the game_map's visible tiles equal to result of compute_fov
+            self.game_map.tiles["transparent"],
+            (self.player.x, self.player.y),
+            radius=8,
+        )
+        # If a tile is "visible" it should be added to "explored"
+        self.game_map.explored |= self.game_map.visible
+
 
     # Draws the screen by iterating through entities and printing each, presenting context, clearing console            
     def render(self, console: Console, context: Context) -> None:
@@ -31,7 +45,9 @@ class Engine:
         self.game_map.render(console)
 
         for entity in self.entities:
-            console.print(entity.x, entity.y, entity.char, fg=entity.color)
+            # Only print entities that are in the FOV
+            if self.game_map.visible[entity.x, entity.y]:
+                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
--- game_map.py
+++ game_map.py
@@ -9,9 +9,23 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F") # Fill self.tiles array with wall tiles (to be carved out later)
 
+        self.visible = np.full((width, height), fill_value=False, order="F") # Tiles the player can currently see
+        self.explored = np.full((width, height), fill_value=False, order="F") # Tiles the player has seen before
+
     def in_bounds(self, x: int, y: int) -> bool: # Returns true as long as the player is within the map boundary
         """Return True if x and y are inside of the bounds of this map"""
         return 0 <= x < self.width and 0 <= y < self.height
     
     def render(self, console: Console) -> None: # Renders the map with the tiles.rgb method
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Renders the map.
+
+        If a tile is in the "visible" array, then draw it with the "light" colors.
+        If it isn't, but it's in the "explored" array, then draw it with the "dark" colors.
+        Otherwise, the default is "SHROUD".
+        """
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select( # np.select replaces just setting tiles to "dark" with conditional tile drawing
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
